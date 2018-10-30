804f8238ff462312a2720f6e074aced028ef9aa2
zjy712 <zjyjwc712@126.com>
2018-10-30 18:34:51 +0800

    update

--- Task5/index.js
+++ Task5/index.js
@@ -1,9 +1,9 @@
 // var newplate = document.createElement('div');
 var plate = {
-    'x' : 30,
-    'y' : 700,
+    'x': 30,
+    'y': 700,
     'height': 100,
-    'width' : 550
+    'width': 550
 }
 var a = {
     name: 'a',
@@ -17,23 +17,20 @@ var c = {
     name: 'c',
     list: []
 }
-var animationsteps = []; //储存动画步骤
+
+var Canvas = document.getElementById('myCanvas');
+var ctx = Canvas.getContext('2d');
 
 $('.btn1').click(function (e) {
     e.preventDefault();
-    var c = document.getElementById('myCanvas');
-    var ctx = c.getContext('2d');
     ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = '2';
     var num = a.list.length;
     var newplate = {};
-    newplate.X = plate.x * (num+1);
-    newplate.Y = plate.y  - (num*plate.height+10);
+    newplate.X = plate.x * (num + 1);
+    newplate.Y = plate.y - (num * plate.height + 10);
     newplate.H = plate.height;
-    newplate.W = plate.width - (num*30*2);
-    ctx.fillRect(newplate.X,newplate.Y, newplate.W, newplate.H);
-    ctx.strokeRect(newplate.X,newplate.Y, newplate.W, newplate.H);
+    newplate.W = plate.width - (num * 30 * 2);
+    ctx.fillRect(newplate.X, newplate.Y, newplate.W, newplate.H);
     a.list.push(newplate);
 
     // var time = setInterval(function () {
@@ -60,6 +57,7 @@ $('.btn1').click(function (e) {
     //     ctx.stroke()
     // },5000)
 });
+
 $('.btn2').click(function (e) {
     e.preventDefault();
     // var platelist = $('.box>div')
@@ -92,35 +90,62 @@ var hanoi = function (n, a, b, c) {
         hanoi(n - 1, b, a, c);
     }
 }
+
 var move = function (n, a, c) {
 
-    var num1 = a.list.length;;
+    var num1 = a.list.length;
 
     var num2 = c.list.length;
     var num3 = c.name.charCodeAt() - a.name.charCodeAt();
     var aCoord = a.list[num1 - 1];
     // var aCoord = $(aplate).css(['bottom', 'left']);
     var upY = 0;
-    var i =0;
-    var x =0;
-    var c = document.getElementById('myCanvas');
-    var ctx = c.getContext('2d');
-    var time = setInterval(function () {
-
-        if (aCoord.Y == upY) {
-            console.log(aCoord.Y);
-            
-            clearInterval(time);
-        } else
-        {
-            console.log(aCoord.Y);
-            i++;
-            x +=5;
-            ctx.clearRect(aCoord.X-2, aCoord.Y+2, aCoord.W+4, aCoord.H);
-            ctx.fillRect(aCoord.X, aCoord.Y-5, aCoord.W, aCoord.H);
-            aCoord.Y -= 5;
-        }
-    }, 1)
+   
+    new Promise(function (resolve, reject) {
+        console.log('Promise1 start');
+        
+        var time = setInterval(function () {
+            if (aCoord.Y == upY) {
+                setTimeout(resolve, 500, aCoord);
+                clearInterval(time);
+                console.log('Promise1 end');
+            } else {
+                ctx.clearRect(aCoord.X, aCoord.Y , aCoord.W, aCoord.H);
+                ctx.fillRect(aCoord.X, aCoord.Y - 5, aCoord.W, aCoord.H);
+                aCoord.Y -= 5;
+            }
+        }, 1);
+        
+    })
+        .then(function (Coord) {
+            console.log('Promise2 start');
+            var upRow = Coord.X + num3 *600;
+            var time2 = setInterval(function () {
+                if (Coord.X == upRow) {
+                    console.log('Promise2 end');
+                    setTimeout(resolve, 100, Coord);
+                    clearInterval(time2);
+                } else {
+                    ctx.clearRect(Coord.X, Coord.Y , Coord.W, Coord.H);
+                    ctx.fillRect(Coord.X + 5*num3, Coord.Y, Coord.W, Coord.H);
+                    Coord.X += 5*num3
+                }
+            }, 1);
+        })
+        .then(function(Coord) {
+            console.log('Promise3 start');
+            var updown= 800- (num2 + 1) * 100;;
+            var time3 = setInterval(function () {
+                if (Coord.Y == updown) {
+                    console.log('Promise3 end');
+                    clearInterval(time3);
+                } else {
+                    ctx.clearRect(Coord.X, Coord.Y , Coord.W, Coord.H);
+                    ctx.fillRect(Coord.X, Coord.Y+5, Coord.W, Coord.H);
+                    Coord.Y += 5
+                }
+            }, 1);
+        })
     // var rowanimate = parseInt(aCoord.left) + num3 * 300;
     // var downanimate = (num2 + 1) * 52;
 
@@ -141,3 +166,10 @@ var move = function (n, a, c) {
 
     // c.list.push(a.list.pop())
 }
+// var movePromise = new Promise(function (resolve, reject) {
+//     console.log('start mew Promise...');
+//     var num1 = a.list.length;
+//     var Coord = a.list[num1 - 1];
+
+//     resolve(Coord);
+// })
